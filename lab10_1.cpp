f5376fb879a18c91b210f946762268b2af548eb3
loyanGunman <raykillerghostth@gmail.com>
2022-12-27 15:34:37 +0700

    lab10_1.cpp Completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,15 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main()
+{
+    double L,R,A ;
 	cout << "Enter initial loan: ";
+    cin >> L;
 	cout << "Enter interest rate per year (%): ";
+    cin >> R;
 	cout << "Enter amount you can pay per year: ";
+    cin >> A;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -21,13 +26,26 @@ int main(){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+int i=1;
+	    do
+	    {
+        if(L == 0)break;
+        cout << fixed << setprecision(2); 
+	    cout << setw(13) << left << i; 
+	    cout << setw(13) << left << L;
+	    cout << setw(13) << left << L*R/100;
+	    cout << setw(13) << left << L+(L*R/100);
+        if (L+(L*R/100) < A)
+        {
+            A = L+(L*R/100);
+        }
+	    cout << setw(13) << left << A ;
+	    cout << setw(13) << left << (L+(L*R/100))-A;
+	    cout << "\n";
+        L = (L+(L*R/100))-A ;
+        i++;
+        }while(L > 1); 
+        
 	return 0;
-}
+	
+	}
